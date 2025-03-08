1718dac20215d987659578343f04b0519bed2513
DzinToDev <maheshdgaikwad18@gmail.com>
2025-03-08 18:45:56 +0530

    revisit useState again created login logout in nav

--- EP 05 Hooked/src/components/Header.js
+++ EP 05 Hooked/src/components/Header.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 const Header = () => {
+
+  // let btnName = "Login"
+  const [btnName, setBtnName] = useState("Login");
+
   return (
-    <nav className=" flex justify-between py-6 ">
+    <nav className=" flex justify-between items-center py-6 ">
       <div className="logo">
         <h1 className="text-[#b28670]">Coffee</h1>
       </div>
-      <div>
+      <div className="nav-right flex gap-10 items-center">
         <ul className="menu flex gap-10">
           <li>
             <a href="/">Home</a>
@@ -16,6 +22,15 @@ const Header = () => {
             <a href="/contact">Contact</a>
           </li>
         </ul>
+        <button className="login px-4 py-2 text-white bg-zinc-400 cursor-pointer"
+        onClick={()=> {
+          // btnName= "LogOut"
+          // setBtnName = "LogOut"; we cant directly modify this for that we have 2nd argument fun 
+          btnName === "Login" ? setBtnName("Log Out") : setBtnName("Login"); // react updated this btn comp and also refresh the header component. it trigger the reconsiliation cycle.
+          console.log(btnName);
+
+        }}
+        >{btnName}</button>
       </div>
     </nav>
   );
